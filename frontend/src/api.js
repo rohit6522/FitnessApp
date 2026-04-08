c4bd778544b4bf610a0c46ee32696517e6bcff31
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-04-08 18:41:24 +0530

    Server Fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-    baseURL: "https://fitnessapp-backend-n0qo.onrender.com/api"  
+baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 })
 
 API.interceptors.request.use((req) => {
